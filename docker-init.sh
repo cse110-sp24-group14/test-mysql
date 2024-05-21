7fae559c83159af118ff54d7d503dac9341541a8
Brenton Dunn <47510212+brentonmdunn@users.noreply.github.com>
2024-05-20 19:34:59 -0700

    Add support for windows

--- docker-init.sh
+++ docker-init.sh
@@ -19,13 +19,13 @@ docker pull mysql
 docker run --name $CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=$MYSQL_PASSWORD -p 3306:3306 -d mysql
 
 # Wait for MySQL to start
-sleep 10
+echo "Waiting for MySQL to initialize..."
+sleep 20
 
 # Copy SQL file into container
 docker cp $SQL_FILE $CONTAINER_NAME:/$SQL_FILE
 
-# Execute SQL script in MySQL container
-docker exec -it $CONTAINER_NAME bash -c " mysql -u$MYSQL_USERNAME -p$MYSQL_PASSWORD < /$SQL_FILE"
+# Execute SQL script in MySQL container using TCP connection
+docker exec -i $CONTAINER_NAME mysql -u$MYSQL_USERNAME -p$MYSQL_PASSWORD --host=127.0.0.1 --port=3306 < $SQL_FILE
 
 echo "Database setup complete!"
-
